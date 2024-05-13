c7b38bc7545db702ce07d09ce57372413318229a
SpeedCash <tcravero@gmail.com>
2024-05-13 10:02:28 +0200

    corrected files

--- javascript-web_scraping/0-readme.js
+++ javascript-web_scraping/0-readme.js
@@ -1,12 +1,18 @@
 #!/usr/bin/node
+
+// Import the filesystem module
 const fs = require('fs');
 
+// Get the file path from the command line arguments
 const filePath = process.argv[2];
 
+// Function to read and print the content of the file
 fs.readFile(filePath, 'utf8', (err, data) => {
   if (err) {
+    // Print error object if an error occurs
     console.error(err);
   } else {
+    // Print the content of the file
     console.log(data);
   }
 });
--- javascript-web_scraping/1-writeme.js
+++ javascript-web_scraping/1-writeme.js
@@ -1,11 +1,16 @@
 #!/usr/bin/node
+
+// Import the filesystem module
 const fs = require('fs');
 
+// Get the file path and the string to write from command line arguments
 const filePath = process.argv[2];
 const content = process.argv[3];
 
-fs.writeFile(filePath, content, 'utf8', err => {
+// Function to write the string to the file
+fs.writeFile(filePath, content, 'utf8', (err) => {
   if (err) {
+    // Print error object if an error occurs during writing
     console.error(err);
   }
 });
