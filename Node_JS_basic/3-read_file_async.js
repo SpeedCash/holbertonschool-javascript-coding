9c0ee96b13a558cd8225df3e1e74d6cf866e6a60
SpeedCash <tcravero@gmail.com>
2024-05-27 11:02:17 +0200

    Using the database database.csv (provided in project description), create a function countStudents in the file 3-read_file_async.js

---
+++ Node_JS_basic/3-read_file_async.js
@@ -0,0 +1,38 @@
+const fs = require('fs');
+
+
+function countStudents(path) {
+  return new Promise((resolve, reject) => {
+    fs.readFile(path, 'utf8', (err, data) => {
+      if (err) {
+        reject(new Error('Cannot load the database'));
+        return;
+      }
+
+      const lines = data.trim().split('\n');
+      const fields = {};
+      lines.shift(); // Remove header
+
+      lines.forEach((line) => {
+        if (line.trim()) {
+          const [firstname, lastname, age, field] = line.split(',');
+          if (!fields[field]) {
+            fields[field] = [];
+          }
+          fields[field].push(firstname);
+        }
+      });
+
+      const totalStudents = lines.length;
+      console.log(\`Number of students: \${totalStudents}\`);
+
+      for (const [field, students] of Object.entries(fields)) {
+        console.log(\`Number of students in \${field}: \${students.length}. List: \${students.join(', ')}\`);
+      }
+
+      resolve();
+    });
+  });
+}
+
+module.exports = countStudents;
