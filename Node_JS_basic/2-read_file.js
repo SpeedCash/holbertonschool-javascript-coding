0f3001f37d3741524864c3fb79b8d61a9d937c47
SpeedCash <tcravero@gmail.com>
2024-05-27 11:49:53 +0200

    updated file

--- Node_JS_basic/2-read_file.js
+++ Node_JS_basic/2-read_file.js
@@ -4,6 +4,8 @@ function countStudents(path) {
   try {
     const data = fs.readFileSync(path, 'utf8');
     const lines = data.trim().split('\n');
+    if (lines.length === 0) throw new Error('Cannot load the database');
+
     const fields = {};
     let totalStudents = 0;
     lines.shift(); // Remove header
