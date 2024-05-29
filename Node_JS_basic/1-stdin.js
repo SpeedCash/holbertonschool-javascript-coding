292b98a55f3aab7e6779ce2bb0990de000b0aa6d
SpeedCash <tcravero@gmail.com>
2024-05-29 09:59:52 +0200

    Create a program named 1-stdin.js that will be executed through command line:

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,7 +1,10 @@
 process.stdout.write('Welcome to Holberton School, what is your name?\n');
+
 process.stdin.on('data', (data) => {
-  const name = data.toString().trim();
-  console.log(`Your name is: ${name}`);
-  process.stdout.write('This important software is now closing\n');
+  process.stdout.write(`Your name is: ${data}`);
   process.exit();
+});
+
+process.on('exit', () => {
+  console.log('This important software is now closing');
 });
